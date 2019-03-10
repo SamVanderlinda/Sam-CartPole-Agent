03f585046d51eba8520b41e77e3318abc2c6bfd1
spokes-8 <48394716+spokes-8@users.noreply.github.com>
2019-03-09 16:21:17 -0800

    Update sam_agent.py

--- sam_agent.py
+++ sam_agent.py
@@ -3,6 +3,7 @@
 #First render is a random agent, performing 200 random micromovements 
 #The agent is then trained without a render
 #Second render is a the trained agent
+#Video demonstration: 
 
 import gym
 import numpy as np
